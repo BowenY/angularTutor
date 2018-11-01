9d96ab9472af1fdbaf1c7ef50034452be0d131e6
BowenY <bzy0014@tigermail.auburn.edu>
2018-10-31 21:32:32 -0400

    testBranch

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,18 +8,12 @@ import { Student } from '../../shared/student.model';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-    serverId = 10;
-    serverStatus = 'offLine';
     counter = 0;
     isLogin = false;
     userName: string;
     students: Student[] = [new Student('Bill Gates', 'Computer Science'),
                            new Student('Steve Jobs', 'Computer Science'),
                            new Student('Elon Musk', 'Computer Science')];
-
-    getServerStatus() {
-      return this.serverStatus;
-    }
 
     counterPlus() {
      this.counter ++;
@@ -35,10 +29,5 @@ export class ServerComponent {
 
     signOut() {
       this.isLogin = false;
-    }
-
-    // Event Binding
-    onUpdateUserName(event: Event) {
-      this.userName = (<HTMLInputElement>event.target).value;
     }
 }
